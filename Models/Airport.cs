b105ddfb134621599df588a57b84edef41408950
AdamCanard <azcunard@gmail.com>
2024-03-06 15:58:32 -0700

    added an airport class, in flight manager i read the airports file and store it in a list. User input should now work for lower case airport codes and you can type the name of the city instead

---
+++ Models/Airport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2FlightReservations.Models
+{
+    internal class Airport
+    {
+        public string Code { get; set; }
+        public string AirportName { get; set; }
+    }
+}
