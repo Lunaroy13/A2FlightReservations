d9b326fb4232e32d89a44f02f7773cbde4d089e5
Simon Luna <simon.lunapatiarroy@edu.sait.ca>
2024-03-07 00:36:46 -0700

    Search flight functionality bugfix, introduce try-catch statements to prevent code breaking, introduce error messages and confirmation messages to display to the user

--- Models/FlightsManager.cs
+++ Models/FlightsManager.cs
@@ -28,78 +28,121 @@ namespace A2FlightReservations.Models
         public static List<Flight> GetFlights() { return flights; }
 
         //given departing airport, arriving airport, and day. returns a list of flight objects that match those critiria
-        public static List<Flight> SearchFlights(string departingInput, string arrivingInput, string day) {
-
-        string departingCode = "";
-        if (departingInput != null) 
+        public static List<Flight> SearchFlights(string departingInput, string arrivingInput, string day) 
         {
-            //if the length of the departing input is 3, we assume that the user has inputted an airport code
-            if (departingInput.Trim().Length == 3)
-            {
-                departingCode = departingInput.Trim().ToUpper();
+            string[] daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-            }
-            //else we assume the user has inputted the name of the airport
-            else
+            string departingCode = "";
+            if (!string.IsNullOrEmpty(departingInput)) 
             {
-                foreach (var airport in airports)
+                //if the length of the departing input is 3, we assume that the user has inputted an airport code
+                if (departingInput.Trim().Length == 3)
                 {
-                    if (airport.AirportName.ToUpper().Contains(departingInput.ToUpper()))
+                    departingCode = departingInput.Trim().ToUpper();
+
+                }
+                //else we assume the user has inputted the name of the airport
+                else
+                {
+                    foreach (var airport in airports)
                     {
-                        departingCode = airport.Code;
-                        break;
+                        if (airport.AirportName.ToUpper().Contains(departingInput.ToUpper()))
+                        {
+                            departingCode = airport.Code;
+                            break;
+                        }
+                    }
+
+                    //if we dont find and airport name that matches the input, search flights based on the user input
+                    if (departingCode == "")
+                    {
+                        departingCode = departingInput.ToUpper();
                     }
                 }
-
-                //if we dont find and airport name that matches the input, search flights based on the user input
-                if (departingCode == "")
-                {
-                    departingCode = departingInput.ToUpper();
-                }
             }
-        }
           
-        string arrivingCode = "";
-        if (arrivingInput != null) 
-        {
-            //if the length of the arriving input is 3, we assume that the user has inputted an airport code
-            if (arrivingInput.Trim().Length == 3)
+            string arrivingCode = "";
+            if (!string.IsNullOrEmpty(arrivingInput)) 
             {
-                arrivingCode = arrivingInput.Trim().ToUpper();
-
-            }
-            //else we assume the user has inputted the name of the airport
-            else
-            {
-                foreach (var airport in airports)
+                //if the length of the arriving input is 3, we assume that the user has inputted an airport code
+                if (arrivingInput.Trim().Length == 3)
                 {
-                    if (airport.AirportName.ToUpper().Contains(arrivingInput.ToUpper()))
+                    arrivingCode = arrivingInput.Trim().ToUpper();
+
+                }
+                //else we assume the user has inputted the name of the airport
+                else
+                {
+                    foreach (var airport in airports)
                     {
-                        arrivingCode = airport.Code;
+                        if (airport.AirportName.ToUpper().Contains(arrivingInput.ToUpper()))
+                        {
+                            arrivingCode = airport.Code;
+                            break;
+                        }
+                    }
+
+                    //if we dont find and airport name that matches the input, search flights based on the user input
+                    if (arrivingCode == "")
+                    {
+                        arrivingCode = arrivingInput.ToUpper();
+                    }
+                }
+            }
+
+            string dayCode = "";
+            if (!string.IsNullOrEmpty(day))
+            {
+                foreach (string weekday in daysOfTheWeek) 
+                {
+                    if (weekday.ToUpper().Contains(day.ToUpper())) 
+                    { 
+                        dayCode = weekday.ToUpper();
                         break;
                     }
                 }
-
-                //if we dont find and airport name that matches the input, search flights based on the user input
-                if (arrivingCode == "")
-                {
-                    arrivingCode = arrivingInput.ToUpper();
-                }
             }
-        }
-            
-
+       
             //Read every flight in flights, return a list of flights that have the same departing and/or arriving airports and/or same day
             // For example, if the user where to enter 'Monday' all Monday flights will be displayed.
             List<Flight> validFlights = new List<Flight>();
             foreach (var flight in flights)
             {
-                if (departingInput == null || (flight.Departing == departingCode) && (arrivingInput == null || flight.Arriving == arrivingCode) && (day == null || flight.Day.ToUpper() == day.ToUpper()))
+                // Check if a value was given for reservationCode
+                if (!string.IsNullOrWhiteSpace(departingInput))
                 {
-                    validFlights.Add(flight);
+                    // Check if reservation matches given code. Skip this reservation otherwise.
+                    if (departingCode != flight.Departing.ToUpper())
+                    {
+                        continue;
+                    }
                 }
+
+                // Check if a value was given for airline
+                if (!string.IsNullOrWhiteSpace(arrivingInput))
+                {
+                    // Check if reservation airline matches given airline. Skip this reservation otherwise.
+                    if (arrivingCode != flight.Arriving.ToUpper())
+                    {
+                        continue;
+                    }
+                }
+
+                // Check if a value was given for travellerName
+                if (!string.IsNullOrWhiteSpace(day))
+                {
+                    // Check if traveller name matches given name. Skip this reservation otherwise.
+                    if (dayCode != flight.Day.ToUpper())
+                    {
+                        continue;
+                    }
+                }
+
+                // Add value to list if we get to this point.
+                validFlights.Add(flight);
             }
-            return validFlights;
+
+        return validFlights;
         }
 
         public static void bookFlight(Flight bookedFlight)
